1063d1ef2df7960a8b7c9d7d0e52b7c25d051c4e
shangqd <shang_qd@qq.com>
2020-01-15 14:16:06 +0800

    add new hash

--- src/crypto/crypto.cpp
+++ src/crypto/crypto.cpp
@@ -116,7 +116,7 @@ uint256 CryptoHash(const uint256& h1, const uint256& h2)
 uint256 CryptoPowHash(int height, const void* msg, size_t len)
 {
     uint256 hash;
-    cn_slow_hash(msg, len, (char*)hash.begin(), 2, 0, 0, height);
+    cn_slow_hash(msg, len, (char*)hash.begin(), 2, 0, 0);
     return hash;
 }
 
